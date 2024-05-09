fb2165cef151609c254e446138bf0cbc6e10b0e5
Willy Ogorzaly <willy@Bestcomuterever.lan>
2024-05-09 14:17:12 -0500

    Add new page nOGs with embedded script

---
+++ src/app/nogs.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const NOGs = () => {
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://mint.highlight.xyz/assets/embed.css" />
+      </Head>
+      <div data-widget="highlight-mint-card" data-mint-collection-id="663cf4174f8e21f1296da1a3"></div>
+      <script type="module" crossorigin="true" src="https://mint.highlight.xyz/assets/embed.js?v=1"></script>
+    </>
+  );
+};
+
+export default NOGs;
