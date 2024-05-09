d3759397c6c39f62f94675e7303b454a2e174f05
Willy Ogorzaly <willy@Bestcomuterever.lan>
2024-05-09 14:22:55 -0500

    Update nogs.tsx

--- src/app/nogs.tsx
+++ src/app/nogs.tsx
@@ -6,7 +6,7 @@ const NOGs = () => {
       <Head>
         <link rel="stylesheet" href="https://mint.highlight.xyz/assets/embed.css" />
       </Head>
-      <div data-widget="highlight-mint-card" data-mint-collection-id="663cf4174f8e21f1296da1a3"></div>
+      <div data-widget="highlight-mint-page" data-mint-collection-id="663d1b360cdee3e13ac85c8d"></div>
       <script type="module" crossorigin="true" src="https://mint.highlight.xyz/assets/embed.js?v=1"></script>
     </>
   );
