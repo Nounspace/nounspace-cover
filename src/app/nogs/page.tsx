6633191ea1d154ba4bdc6c65a772b84022f77b82
Julian Early <julian.is.early@gmail.com>
2024-05-15 13:14:01 -0700

    fix: make routing and import look alright for nogs page

--- src/app/nogs/page.tsx
+++ src/app/nogs/page.tsx
@@ -7,7 +7,7 @@ const NOGs = () => {
         <link rel="stylesheet" href="https://mint.highlight.xyz/assets/embed.css" />
       </Head>
       <div data-widget="highlight-mint-page" data-mint-collection-id="663d1b360cdee3e13ac85c8d"></div>
-      <script type="module" crossorigin="true" src="https://mint.highlight.xyz/assets/embed.js?v=1"></script>
+      <script type="module" crossOrigin="" src="https://mint.highlight.xyz/assets/embed.js?v=1" async></script>
     </>
   );
 };
