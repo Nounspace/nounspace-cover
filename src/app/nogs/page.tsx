8fd15368048014746ae46dd95d4423f15accdb3b
Julian Early <julian.is.early@gmail.com>
2024-05-15 15:41:01 -0700

    fix: update nogs/page to load properly using next script

--- src/app/nogs/page.tsx
+++ src/app/nogs/page.tsx
@@ -1,12 +1,16 @@
-"use client"; 
+"use client"
+
+import Script from "next/script";
 
 const NOGs = () => {
+  global.Buffer = Buffer;
+
   return (
     <>
       <link rel="stylesheet" href="https://mint.highlight.xyz/assets/embed.css" />
       <main className="flex min-h-screen flex-col items-center justify-between bg-white">
         <div data-widget="highlight-mint-page" data-mint-collection-id="663d2717dffb7b3a490f398f" ></div>
-        <script async={true} type="module" crossOrigin="" src="https://mint.highlight.xyz/assets/embed.js?v=1"></script>
+        <Script type="module" crossOrigin="" src="https://mint.highlight.xyz/assets/embed.js?v=1"/>
       </main>
     </>
   );
